587b5342ab2df6d80f45a08c0c81c7d922c5f1f3
SquidFruit <gup.kregor@hotmail.com>
2014-04-04 17:49:15 +1100

    Welcome Update

--- src/com/squidfruit/swornannouncer/PlayerJoin.java
+++ src/com/squidfruit/swornannouncer/PlayerJoin.java
@@ -1,20 +1,26 @@
 package com.squidfruit.swornannouncer;
 
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
 import org.bukkit.FireworkEffect.Type;
+import org.bukkit.Material;
 import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.FireworkMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class PlayerJoin implements Listener{
 	
 	private SwornAnnouncer plugin;
+	
+	
 
 	public PlayerJoin(SwornAnnouncer plugin)
 	{
@@ -23,17 +29,54 @@ public class PlayerJoin implements Listener{
 	
 	
 	
-	
 	@EventHandler
 	public void onPlayerJoin(final PlayerJoinEvent pje) {
+        boolean MessageOnJoin = plugin.getConfig().getBoolean("onlineevent");
+		if (MessageOnJoin)
+		{		
+       	 Player player = pje.getPlayer();
+       	 
+       	 	// does the new feture (adds welcome message in itembox)
+       	 	 final ItemStack inv = new ItemStack(player.getItemInHand());
+			 final ItemStack stack = new ItemStack(Material.DIRT, 1);
+			 final ItemStack air = new ItemStack(Material.AIR);
+			 
+			 
+			 
 			 Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable(){
 	             public void run(){
-	            	 Player playername = pje.getPlayer();
-                     boolean MessageOnJoin = plugin.getConfig().getBoolean("onlineevent");
-             		if (MessageOnJoin)
-             		{
+	            	 Player player = pje.getPlayer();
+	            	 
+	            	 
+		 				ItemMeta meta = stack.getItemMeta();
+		 				meta.setDisplayName(ChatColor.GREEN + "Welcome To The Server " + player.getName());
+		 				stack.setItemMeta(meta);
+		 				
+	            	 
+	            	 if (player.getItemInHand().getType() == Material.AIR) {
+		 	 				player.getInventory().setItemInHand(stack);
+	            		 Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable(){
+	        	             public void run(){
+	        	               	 Player player = pje.getPlayer();
+	        	               	 player.setItemInHand(air);
+	        	            	 
+	 	 				
+	        	             }
+	 	 				
+	        	             }, 35);
+	            		 
+	            	 }
+	            	 
+
+	 				player.getInventory().setItemInHand(stack);
+	 				
+	            	 
+	            	 
+	            	 
+	            	 
+	            	 
              		for (Player player2 : plugin.getServer().getOnlinePlayers()) {
-             			player2.sendMessage(ChatColor.AQUA + playername.getName() + "'s " + plugin.getConfig().getString("onjoinmessage")
+             			player2.sendMessage(ChatColor.AQUA + player.getName() + "'s " + plugin.getConfig().getString("onjoinmessage")
              					.replaceAll("(&([a-f0-9]))", "\u00A7$2"));
                      Firework f = (Firework) pje.getPlayer().getWorld().spawn(pje.getPlayer().getLocation(), Firework.class);
                      Firework f2 = (Firework) pje.getPlayer().getWorld().spawn(pje.getPlayer().getLocation(), Firework.class);
@@ -80,16 +123,39 @@ public class PlayerJoin implements Listener{
                      
                      
                     
-             }
-     }, 20);
+             
+     }, 1);
+			 
+			 
+			 Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable(){
+	             public void run(){
+	            	 
+	            	 Player player = pje.getPlayer();
+	            	 player.getInventory().remove(stack);
+	            	 player.setItemInHand(inv);
+	            	 
+	            	 
+	             
+	             }
+			 
+			 
+			 }, 35);
 			 }
-		}
+	}
+	}
+		
 	
 
 			
 			
 		
 		
+		
+	  
+
+
+
+
 		
 	  
 
